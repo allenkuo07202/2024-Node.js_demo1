e0312c13481fec97da70306afa25cd456024f7a0
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-12 21:21:28 +0800

    response內容為string

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const http = require("http"); //引入http這個module
 // 及response object(代表把http response的內容作成一個object，還外加一些其他的屬性)
 const server = http.createServer((req, res) => {
   // console.log(req.headers);
-  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-  // 設定header裡面的內容
-  res.write("歡迎來到我的網頁");
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" }); // 設定header裡面的內容
+  // console.log(req.url);// 可根據這個判斷，要顯示給使用者的內容是什麼
+  if (req.url == "/") {
+    res.write("歡迎來到我的網頁");
+  } else if (req.url == "/anotherPage") {
+    res.write("這是另一個頁面");
+  } else {
+    res.write("不存在的頁面");
+  }
   res.end();
 }); // callback function with 2 parameters
 
