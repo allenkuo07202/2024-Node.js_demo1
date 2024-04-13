db37f6126158a55e103fb0c59e8f0eabe2646bd9
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-13 20:56:40 +0800

    載入npm package

--- app.js
+++ app.js
@@ -1,38 +1,12 @@
-const http = require("http"); //引入http這個module
-// 想把網頁透過response送到client端，則需讀取該網頁這個文件
-const fs = require("fs"); // 引入fs(file system)這個module
-// 用http.createServer()創建網頁伺服器，並在createServer()內給callback function with 2 parameters
-// 該callback function會自動被Node.js執行
-// 此時，會創建request object(代表把http request的內容作成一個object，還外加一些其他的屬性)
-// 及response object(代表把http response的內容作成一個object，還外加一些其他的屬性)
-const server = http.createServer((req, res) => {
-  // console.log(req.headers);
-  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" }); // 設定header裡面的內容
-  // console.log(req.url);// 可根據這個判斷，要顯示給使用者的內容是什麼
-  if (req.url == "/") {
-    res.write("歡迎來到我的網頁");
-    res.end();
-  } else if (req.url == "/anotherPage") {
-    res.write("這是另一個頁面");
-    res.end();
-  } else if (req.url == "/myFile") {
-    fs.readFile("myFile.html", (e, data) => {
-      if (e) {
-        res.write("存取html文件錯誤");
-        res.end();
-      } else {
-        res.write(data);
-        res.end();
-      }
-    });
-  } else {
-    res.write("不存在的頁面");
-    res.end();
-  }
-}); // callback function with 2 parameters
+// 在package.json裡面，dependencies內的東西，可以直接require
+const cowsay = require("cowsay");
 
-// 只要啟動伺服器，就會聆聽是否有request請求
-server.listen(3000, () => {
-  console.log("伺服器正在port3000上運行");
-});
-// 把http request的內容作成一個object，還外加一些其他的屬性
+console.log(
+  cowsay.say({
+    text: "我是一隻大牛",
+    e: "oO",
+    T: "U ",
+  })
+);
+
+// or cowsay.think()
